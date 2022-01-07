5d87510157e4d0d6612153c5fa728882c26d1df4
WenrrrBeth <bethding0923@gmail.com>
2022-01-06 20:17:14 -0700

    timer fixed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import time
+import os
 
 client = discord.Client()
 
@@ -9,29 +10,34 @@ async def on_ready():
 
 
 def timer(set_time):
+    set_time = set_time*60
     while set_time:
-        mins = set_time
-        print(mins, end="\r")
+        mins, secs = divmod(set_time, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
         time.sleep(1)
         set_time -= 1
+
 
 @client.event
 # checks messages if from the bot itself
 async def on_message(message):
     if message.author == client.user:
       return
-    
-
+      
     # if user sends something to bot
     if message.content.startswith('-start'):
         await message.channel.send('Hello! starting the Pomodoro now!')
-        set = await(message.channel.send('Enter the time in Minutes you would like:'))
-        print(set)
-      
-        if message.content.startswith('-set'):
-            timer(set)
-            await message.channel.startswith('your time has been set to', set, '. Happy Studying!')
+        await message.channel.send('Enter the time in Minutes you would like:') 
+        # set = await(message.channel.send('Enter the time in Minutes you would like:'))
+        # print(set)
+        
+    if message.content.startswith('-set'):
+        # timer(set)
+        # set = await(message.channel.send('Enter the time in Minutes you would like:'))
+        set = int(message.content.split(' ')[1])
+        await message.channel.send('your time has been set to ' + str(set) + ' Happy Studying!')
+        timer(set)
         
 # running the bot with token
-
 
