2e5ffbf1a6d542eb10c974665bc4f92d11be8b50
WenrrrBeth <bethding0923@gmail.com>
2022-01-07 17:36:41 -0700

    pause and stop feature

--- main.py
+++ main.py
@@ -4,46 +4,136 @@ import os
 
 client = discord.Client()
 
-class Param:
+pau = False
+
+class Clock:
     def __init__(self):
         self.mins = 0
         self.secs = 0
+        self.pause = False
+        self.stop = False
+
+    def setMin(self, min):
+        self.mins = min
+
+    def getMin(self):
+        return self.mins
+    
+    def setSec(self, sec):
+        self.secs = sec
+
+    def getSec(self):
+        return self.secs
+
+    async def setPause(self, pau):
+        print("executed")
+        self.pause = pau 
+        print("pause", self.pause)
+    
+    def getPause(self):
+        return self.pause
+
+    def setStop(self, sto):
+        self.stop = sto
+    
+    def getStop(self):
+        return self.stop
+
+    async def starttimer(self, message):
+        # set_time = set_time*60
+        # 
+        # mins = 0
+        # secs = 0
+        set_time = self.mins*60 + self.secs
+        msg = await message.channel.send('{:02d}:00'.format(self.mins))
+        mins = 0
+        secs = 0
+        while set_time:
+            # mins, secs = divmod(set_time, 60)
+            print(pau)
+            if not pau:
+                # mins = set_time//60
+                # secs = set_time%60
+                self.mins = set_time//60
+                self.secs = set_time%60
+                timer = '{:02d}:{:02d}'.format(self.mins, self.secs)
+                print(timer, end="\r")
+                await msg.edit(content="Time remaining: " + str(timer))
+                time.sleep(1)
+                set_time -= 1
+            elif pau:
+                await message.channel.send('Your session has paused at {:02d}:{:02d}'.format(self.mins, self.secs))
+                await message.channel.send('To resume type -r')
+                return
+            elif self.stop:
+                await message.channel.send('Your session has stopped')
+                await message.channel.send('To Start another set, type -set[enter time in minutes]')
+                self.mins = 0
+                self.secs = 0
+                self.pause = 0
+                self.stop = 0
+                return
+
+
+        if not set_time:
+            await msg.edit(content="Time remaining: 00:00")
+            await message.channel.send('Your session has ended. Great Work!')
+            await message.channel.send('To Start another set, type -set[enter time in minutes]')
+            await message.channel.send('For list of commands type -c')
 
 @client.event
 async def on_ready():
     print('welcome to {0.user}!'.format(client))
 
-async def timer(min, sec, message, pause=False):
-    # set_time = set_time*60
-    # 
-    # mins = 0
-    # secs = 0
-    set_time = min*60
-    msg = await message.channel.send('{:02d}:00'.format(min))
-    mins = 0
-    secs = 0
-    while set_time:
-        if not pause:
-            # mins, secs = divmod(set_time, 60)
-            mins = set_time//60
-            secs = set_time%60
-            timer = '{:02d}:{:02d}'.format(mins, secs)
-            print(timer, end="\r")
-            await msg.edit(content="Time remaining: " + str(timer))
-            time.sleep(1)
-            set_time -= 1
+# async def timer(param, message, pause=False, stop=False):
+#     # set_time = set_time*60
+#     # 
+#     # mins = 0
+#     # secs = 0
+#     set_time = param.getMin()*60 + param.getSec()
+#     msg = await message.channel.send('{:02d}:00'.format(param.getMin()))
+#     mins = 0
+#     secs = 0
+#     while set_time:
+#         # mins, secs = divmod(set_time, 60)
+#         print(param.getPause())
+#         if not param.getPause():
+#             # mins = set_time//60
+#             # secs = set_time%60
+#             param.setMin(set_time//60)
+#             param.setSec(set_time%60)
+#             timer = '{:02d}:{:02d}'.format(param.getMin(), param.getSec())
+#             print(timer, end="\r")
+#             await msg.edit(content="Time remaining: " + str(timer))
+#             time.sleep(1)
+#             set_time -= 1
+#         elif param.getPause():
+#             await message.channel.send('Your session has paused at {:02d}:{:02d}'.format(mins, secs))
+#             await message.channel.send('To resume type -r')
+#             return
+#         elif param.getStop():
+#             await message.channel.send('Your session has stopped')
+#             await message.channel.send('To Start another set, type -set[enter time in minutes]')
+#             param.setMin(0)
+#             param.setSec(0)
+#             param.setPause(False)
+#             param.setStop(False)
+#             return
 
-        else:
-            await message.channel.send('You have paused the at ' + mins + ':' + secs)
-            return mins, secs
+
+#     if not set_time:
+#         await msg.edit(content="Time remaining: 00:00")
+#         await message.channel.send('Your session has ended. Great Work!')
+#         await message.channel.send('To Start another set, type -set[enter time in minutes]')
+#         await message.channel.send('For list of commands type -c')
             
-        
 
 @client.event
 # checks messages if from the bot itself
 async def on_message(message):
-    param = Param()
-    param.mins +=1
+    clock = Clock()
+
+    pause = False
     
     if message.author == client.user:
       return
@@ -63,12 +153,52 @@ async def on_message(message):
     if message.content.startswith('-set'):
         # timer(set)
         # set = await(message.channel.send('Enter the time in Minutes you would like:'))
-        set = int(message.content.split(' ')[1])
-        await message.channel.send('Your time has been set to ' + str(set) + ' minutes. Happy Studying!')
-        await timer(set, 0, message)
+        # clock.setMin(int(message.content.split(' ')[1]))
+        await message.channel.send('Your time has been set to ' + str(clock.getMin()) + ' minutes. Happy Studying!')
+        # await clock.starttimer(message)
+
+        getMin = int(message.content.split(' ')[1])
+        getSec = 0
+        
+        set_time = getMin*60 + getSec
+
+        msg = await message.channel.send('{:02d}:00'.format(getMin))
+        mins = 0
+        secs = 0
+        while set_time:
+            # mins, secs = divmod(set_time, 60)
+            # print(param.getPause())
+            print(pause)
+            if not pause:
+                # mins = set_time//60
+                # secs = set_time%60
+                getMin = set_time//60
+                getSec = set_time%60
+                timer = '{:02d}:{:02d}'.format(getMin, getSec)
+                print(timer, end="\r")
+                await msg.edit(content="Time remaining: " + str(timer))
+                time.sleep(1)
+                set_time -= 1
+            elif pause:
+                await message.channel.send('Your session has paused at {:02d}:{:02d}'.format(mins, secs))
+                await message.channel.send('To resume type -r')
+                return
+
+
+        if not set_time:
+            await msg.edit(content="Time remaining: 00:00")
+            await message.channel.send('Your session has ended. Great Work!')
+            await message.channel.send('To Start another set, type -set[enter time in minutes]')
+            await message.channel.send('For list of commands type -c')
 
     if message.content.startswith('-pause'):
-        await timer(set, message, True)
+        pause = True
+
+    if message.content.startswith('-r'):
+        clock.setPause(False)
+
+    if message.content.startswith('-stop'):
+        clock.setStop(True)
         
 # running the bot with token
 # print(os.environ.get('TOKEN'))
